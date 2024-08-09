7c27631ba75f4897b797c46b24bf989c4e9d503a
LucasCarneiro99 <lucasbcarneiro903@gmail.com>
2024-08-09 09:05:58 -0300

    ënvio de alteracoes

--- src/entities/Main.java
+++ src/entities/Main.java
@@ -1,10 +1,57 @@
 package entities;
 
-public class Main {
+import java.util.*;
 
+public class Main {
+	
+	public void mostrarConteudo() {
+		
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello");
+		
+		String usuario = "adm";
+		String senha = "123";
+		Scanner sc = new Scanner(System.in);
+		int numeroEscolhido = 0;
+		
+		/*Fluxo:
+		 * mostrar menu principal,
+		 * caso aperte 1 - mostrar o menu principal
+		 * caso aperte 2 - listar conteudo
+		 * caso aperte 4 finalizar programa
+		 *
+		 *
+		 */
+		
+		
+		
+		do{
+			System.out.println("Menu Principal: ");
+			System.out.println("1. Login: Fazer login no sistema.");
+			System.out.println("2. Listar Conteúdos: Listar os conteúdos.");		
+			System.out.println("3. Sair: Sair do sistema.");
+			
+			numeroEscolhido = sc.nextInt();
+			
+			switch(numeroEscolhido){
+				case 1:
+					System.out.println("Digite o usuário");
+					String temp_usuario = sc.next();
+					System.out.println("Digite a senha");
+					String temp_senha = sc.next();
+					
+					break;
+				case 2:
+					break;
+				case 3:
+					break;
+				default:System.out.println("Opcao Invalida!");
+			};
+		}while(numeroEscolhido != 4);
+		
+		
+		sc.close();
 	}
 
 }
